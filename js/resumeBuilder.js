b2d5a005d434461fadd91b40f5797e306d89e3fd
Joseph Alan Chandler Jr <josephalanchandlerjr@gmail.com>
2018-02-07 19:01:02 -0500

    append contact types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,5 +3,10 @@ var headerName = $(HTMLheaderName);
 var headerRole = $(HTMLheaderRole);
 header.prepend(headerName);
 headerName.after(HTMLheaderRole);
- 
+
+var topContacts = $('#topContacts');
+contactTypes = [HTMLcontactGeneric, HTMLmobile, HTMLemail, HTMLtwitter, HTMLgithub, HTMLblog, HTMLlocation];
+contactTypes.forEach(function(item){
+	topContacts.append(item);
+});
  
